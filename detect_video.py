eb9c1e05a56029a5fd903144d75a8bb96a70815b
aashay987 <wastemailaap@gmail.com>
2021-05-21 19:33:07 +0530

    First commit

--- detect_video.py
+++ detect_video.py
@@ -1,4 +1,6 @@
 import time
+#import pyttsx3
+import speech_recognition as sr
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
@@ -26,6 +28,14 @@ flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when sav
 flags.DEFINE_float('iou', 0.45, 'iou threshold')
 flags.DEFINE_float('score', 0.25, 'score threshold')
 flags.DEFINE_boolean('dont_show', False, 'dont show video output')
+
+def speak_now(speech_text):
+    engine = pyttsx3.init()
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', 125)
+    #print(engine.getProperty('rate'))
+    engine.say(speech_text)
+    engine.runAndWait()
 
 def main(_argv):
     config = ConfigProto()
@@ -70,7 +80,7 @@ def main(_argv):
         else:
             print('Video has ended or failed, try a different video format!')
             break
-    
+
         frame_size = frame.shape[:2]
         image_data = cv2.resize(frame, (input_size, input_size))
         image_data = image_data / 255.
@@ -110,17 +120,123 @@ def main(_argv):
         result = np.asarray(image)
         cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
         result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        
+
+        ######################
+        image_h, image_w, _ = frame.shape
+        out_boxes, out_scores, out_classes, num_boxes = pred_bbox
+        classes=['Scalpel','Straight_dissection_clamp','Straight_mayo_scissor','Curved_mayo_scissor']
+        for i in range(num_boxes[0]):
+            if int(out_classes[0][i]) < 0 or int(out_classes[0][i]) > 4: continue
+            coor = out_boxes[0][i]
+            coor[0] = int(coor[0] * image_h)
+            coor[2] = int(coor[2] * image_h)
+            coor[1] = int(coor[1] * image_w)
+            coor[3] = int(coor[3] * image_w)
+
+            fontScale = 0.5
+            score = out_scores[0][i]
+            class_ind = int(out_classes[0][i])
+            if(X==class_ind and score> 85):
+                print(coor[0],coor[1],coor[2],coor[3])
+                print(classes[class_ind])
+                print(score,end="%")
+                print("")
+                print("#########")
+        #########################
+
         if not FLAGS.dont_show:
             cv2.imshow("result", result)
-        
+
         if FLAGS.output:
             out.write(result)
         if cv2.waitKey(1) & 0xFF == ord('q'): break
     cv2.destroyAllWindows()
 
+# def speech():
+#     r2 = sr.Recognizer()
+#     r3 = sr.Recognizer()
+
+#     #times = 100
+#     while True:
+#         with sr.Microphone() as source :
+#             print("Speak Now")
+#             #speak_now("Speak Now")
+#             r3.adjust_for_ambient_noise(source)
+#             audio = r3.listen(source)
+#             try:
+#                 if 'hello' in r3.recognize_google(audio):
+#                     with sr.Microphone() as source :
+#                         print("Search your query")
+#                         #speak_now("Search your query")
+#                         r2.adjust_for_ambient_noise(source)
+#                         audio = r2.listen(source)
+
+#                         try:
+#                             get = r2.recognize_google(audio)
+#                             print(get)
+#                         except sr.RequestError as e:
+#                             print('Failed'.format(e));
+#                 elif 'exit' in r3.recognize_google(audio):
+#                     print('Exit')
+#                     #speak_now("Buh bye!")
+#                     break
+#                 else:
+#                     continue
+#             except:
+#                 continue
+
+
+r3 = sr.Recognizer()
+r2 = sr.Recognizer()
+#speech()
+def speech():
+
+    print("Search your query")
+    #speak_now("Search your query")
+    while True:
+        with sr.Microphone() as source :
+            r2.adjust_for_ambient_noise(source)
+            audio = r2.listen(source,timeout =5)
+            try:
+                get = r2.recognize_google(audio,language = "en-GB")
+                #print(get)
+                x=get.lower()
+                return x
+            except sr.RequestError as e:
+                print('Failed'.format(e))
+
+            except sr.WaitTimeoutError:
+                print('timeout')
+            except:
+                print("Please Repeat")
+                #speak_now("please repeat")
+
+dict={"scalpel":0,"pal pal":0,"clamp":1,"scissors":2,"scissor":2}
+X=0
+
 if __name__ == '__main__':
     try:
-        app.run(main)
+        while True:
+            with sr.Microphone() as source :
+                r3.adjust_for_ambient_noise(source)
+                audio = r3.listen(source)
+                try:
+                    if 'hello' in r3.recognize_google(audio):
+                        x=speech()
+
+                        print(x)
+                        if x in dict.keys():
+                            X=dict[x]
+                            print(str(X))
+                            #image()
+                            app.run(main)
+                    elif 'exit' in r3.recognize_google(audio):
+                        print('Exit')
+                        #speak_now("Good bye")
+                        break
+                except:
+                    continue
+
+
     except SystemExit:
         pass
